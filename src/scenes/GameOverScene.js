e21d92c5d500c14b3ca075baa16de14af9eb6e1a
ming <ming@mings-MacBook-Pro.local>
2026-04-26 10:11:48 +1000

    chore: noEmit + ignore tsc-emitted .js artifacts

--- src/scenes/GameOverScene.js
+++
@@ -1,4 +0,0 @@
-import Phaser from 'phaser';
-export class GameOverScene extends Phaser.Scene {
-    constructor() { super('GameOverScene'); }
-}
--- src/scenes/PauseScene.js
+++
@@ -1,4 +0,0 @@
-import Phaser from 'phaser';
-export class PauseScene extends Phaser.Scene {
-    constructor() { super('PauseScene'); }
-}
